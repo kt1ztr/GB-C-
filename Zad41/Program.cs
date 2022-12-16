c7f2d6fd6f73ad9dee339d90b2d66a4380b11ac4
kt1ztr <kata1u3atop@gmail.com>
2022-12-16 11:38:53 +0300

    lesson 6, zadaniya 41 & 43

---
+++ Zad41/Program.cs
@@ -0,0 +1,23 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+//Вводим сразу все числа в одну линию, не через бесконечное кол-во ReadLine.
+Console.Clear();
+Console.WriteLine("Введите колличество элементов в одномерном массиве");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите все элементы массива черрез /. Напоминаю их !>{m}<!");
+double[] array = Console.ReadLine().Split('/').Select(х => double.Parse(х)).ToArray();
+Console.WriteLine($"В массиве [{string.Join("|",array)}] - чисел больше 0 -> {Pol(m,array)}");
+int Pol (int m, double[] array)
+{
+    int col = 0;
+    for (int i=0; i<m; i++)
+    {
+        if (array[i] > 0 )
+        {
+            col++;
+        }
+    }
+    return col;
+}
---
+++ Zad43/Program.cs
@@ -0,0 +1,40 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//Точки для уравнений пользователь вводит с клавиатуры, можно через несколько ReadLine
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.Clear();
+Console.WriteLine("Находим точку пересечения двух прямых. Введите четыре координаты двух прямых.");
+Console.WriteLine("Введите k1:");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b1:");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2:");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2:");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double Peres(double k1, double k2, double b1, double b2)
+{
+    double x = 0;
+    if (k1 == k2)
+    {
+        if (b1 == b2)
+        {
+            Console.WriteLine("Прямые совпадают и имеют бесконечное количество точек пересечения.");
+        }
+        else
+        {
+            Console.WriteLine("Прямые параллельны и нет точек пересечения.");
+        }
+    }
+    else 
+    {
+        x = (b2-b1)/(k1-k2);
+    }
+    return x;
+}
+if (Peres(k1,k2,b1,b2) != 0)
+{
+    Console.WriteLine($"Прямые, заданные уровнениями y = {k1} * x + {b1}, y = {k2} * x + {b2} имеют одну точку пересечения. Ее координаты ({Peres(k1,k2,b1,b2)};{k1*Peres(k1,k2,b1,b2)+b1}).");
+}
