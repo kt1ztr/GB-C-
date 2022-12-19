4c5c1670c740b6866cd344543d80750d8bb0fc1d
kt1ztr <kata1u3atop@gmail.com>
2022-12-19 15:18:24 +0300

    456789fkjmvfhbhf

---
+++ zad66/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+//Задания выполняются только с помощью рекурсий!
+Console.Clear();
+Console.WriteLine("Первое число");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+if (m<n)
+{
+    NumSum(m,n,sum);
+}
+else
+{
+    NumSum(n,m,sum);
+}
+
+
+void NumSum(int m, int n, int sum)
+{
+    if (m>n)
+    {
+        Console.WriteLine($" -> Их сумма равна {sum}");
+        return;
+    }
+    sum = sum + m;
+    Console.Write(m +" ");
+    m++;
+    NumSum(m,n,sum);
+}
---
+++ zad68/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+//m = 3, n = 2 -> A(m,n) = 29
+Console.Clear();
+Console.WriteLine("Первое число");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Результат вычисления функции Аккермана для {m} и {n} равно {Akkerman(m,n)}");
+int Akkerman(int m, int n)
+{
+    if (m == 0) 
+    {
+        return n+1;
+    }
+    if (m != 0 && n == 0) 
+    {
+        return Akkerman(m-1,1);
+    }
+    if (m > 0 && n > 0)   
+    {
+        return Akkerman(m-1,Akkerman(m,n-1));
+    }
+    return Akkerman(m,n);
+}
